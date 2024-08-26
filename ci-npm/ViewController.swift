5c0ff0f88418e55b5acacc44bb3b359473253725
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-08-26 00:38:53 -0700

    Initial Commit

---
+++ ci-npm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ci-npm
+//
+//  Created by Dustin Likens on 8/26/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
