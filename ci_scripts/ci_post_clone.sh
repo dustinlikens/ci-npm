ca6e0d880aa40931e4e01627ba06f3391bb1bed7
dustinlikenssch <dustin.likens@seattlechildrens.org>
2024-08-26 01:03:58 -0700

    update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,5 +2,11 @@
 
 brew install npm
 npm init -y
-npm i --save fs jsonwebtoken axios request zlib csvtojson
+npm i fs
+npm i axios
+npm i jsonwebtoken
+npm i request
+npm i zlib
+npm i out
+npm i csvtojson
 node test.js
